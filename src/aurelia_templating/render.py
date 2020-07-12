378f238c4e00146a5ed148d938609158c710765f
Julien Enselme <jenselme@jujens.eu>
2020-07-12 21:32:01 +0200

    feat(render): add basic support for string interpolation

--- src/aurelia_templating/render.py
+++ src/aurelia_templating/render.py
@@ -1,8 +1,22 @@
+import re
+
+
 def render_string(template_string: str, context: dict) -> str:
     """Render the template string passed as a parameter with the supplied context.
     """
     output = ''
     for line in template_string.split('\n'):
-        output += line + '\n'
+        processed_line = _process_line(line, context)
+        output += processed_line + '\n'
 
     return output
+
+
+def _process_line(line, context):
+    # Process variable interpolation.
+    all_variables = re.findall(r'\$\{(\w+)\}', line)
+    print(all_variables)
+    for variable_name in all_variables:
+        line = line.replace(fr'${{{variable_name}}}', context.get(variable_name, ''))
+
+    return line
--- tests/test_basic_rendering.py
+++ tests/test_basic_rendering.py
@@ -1,5 +1,7 @@
 from aurelia_templating import render_string
 
+import pytest
+
 
 def test_no_syntax():
     input = 'Coucou toi'
@@ -7,3 +9,18 @@ def test_no_syntax():
     output = render_string(input, {})
 
     assert f'{input}\n' == output
+
+
+@pytest.mark.parametrize(
+    "template,context,expected",
+    [
+        ("Hi ${name}", {'name': 'Julien'}, "Hi Julien\n"),
+        ("Hi ${name}, how are you ${name}?", {'name': 'Julien'}, "Hi Julien, how are you Julien?\n"),
+        ("Hi ${name}", {}, "Hi \n"),
+        ("Hi ${name1} and ${name2}!", {'name1': 'Julien', 'name2': 'Pierre'}, "Hi Julien and Pierre!\n"),
+    ]
+)
+def test_variable_interpolation(template, context, expected):
+    output = render_string(template, context)
+
+    assert output == expected
